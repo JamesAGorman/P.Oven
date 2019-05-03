6c35094bc01b99731d88406731ece103b3c8bd7d
leonarr1 <leonarr1@tcd.ie>
2019-05-03 02:32:41 +0100

    quick fix

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -26,7 +26,6 @@ import CenteredSection from './CenteredSection';
 import Form from './Form';
 import Input from './Input';
 import Section from './Section';
-import TransSection from './TransSection'
 import messages from './messages';
 import { loadRepos } from '../App/actions';
 import { changeUsername } from './actions';
